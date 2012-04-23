7e261032866ba6dff4c5373d191f38f07091d0a6
r0q <deen@ti.elem>
2012-04-23 02:05:23 +0200

    infinite tree

--- vis.hs
+++ vis.hs
@@ -2,11 +2,14 @@ import GHC.HeapView
 import Control.Monad
 import Data.Tree
 
---buildTree :: a -> (Closure, [Closure])
---buildTree x = (y, [mapM buildTree $ liftM allPtrs y])
---  where y = getBoxedClosureData x
 buildTree x = do
   cd <- getBoxedClosureData x
   return (cd, allPtrs cd)
 
 t x = unfoldTreeM buildTree x
+u = t . asBox
+
+-- infinite tree duh
+f = let y = id (:) () y
+    in getClosureData y
+
