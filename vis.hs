44de09cb15515ce05bf5038977f38d33c4b417ff
r0q <deen@cr.elem>
2012-04-24 19:00:13 +0200

    tries

--- vis.hs
+++ vis.hs
@@ -6,6 +6,8 @@ import Data.Tree
 import Data.Word
 import GHC.Prim
 import GHC.Exts
+
+import Data.IntMap(IntMap)
 import qualified Data.IntMap as IntMap
 
 buildTree x = do
@@ -23,11 +25,27 @@ f = let y = id (:) () y
 boxAddr :: Box -> Int
 boxAddr (Box any) = fromIntegral $ W# (aToWord# any)
 
-ins :: Box -> Closure -> IntMap.IntMap Closure -> IntMap.IntMap Closure
+ins :: Box -> Closure -> IntMap Closure -> IntMap Closure
 ins box closure im = IntMap.insert (boxAddr box) closure im
 
-walkHeap a = go (asBox a)
-  where go b = do
-          c <- getBoxedClosureData b
-          ims <- liftM IntMap.unions (sequence (map go (allPtrs c)))
-          return $ (ins b c ims)
+walkHeap a = go (asBox a) IntMap.empty
+  where go b im | (boxAddr b) `IntMap.member` im = do
+                    return im
+                | otherwise = do
+                    c <- getBoxedClosureData b
+                    --ims <- liftM IntMap.unions (sequence (map (\x -> go x im) (allPtrs c)))
+                    ims <- foldM (\im x -> go x im) im (allPtrs c)
+                    return $ (ins b c ims)
+
+walkHeapDepth a d = go (asBox a) d IntMap.empty
+  where go b d im | d == 0 = return im
+                  | (boxAddr b) `IntMap.member` im = do
+                      return im
+                  | otherwise = do
+                      c <- getBoxedClosureData b
+                      --ims <- liftM IntMap.unions (sequence (map (\x -> go x im) (allPtrs c)))
+                      ims <- foldM (\im x -> go x (d - 1) im) im (allPtrs c)
+                      return $ (ins b c ims)
+
+--nameGraph :: IntMap Closure -> [(String, [String])]
+--nameGraph im = 
