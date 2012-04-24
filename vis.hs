303e1b3c99dd4aa87ac0961e1c45c2328b699894
r0q <deen@cr.elem>
2012-04-24 12:20:33 +0200

    add walkHeap

--- vis.hs
+++ vis.hs
@@ -1,12 +1,27 @@
+{-# LANGUAGE MagicHash, BangPatterns #-}
+
 import GHC.HeapView
 import Control.Monad
 import Data.Tree
+import Data.Word
+import GHC.Prim
+import GHC.Exts
+import qualified Data.IntMap as IntMap
 
---buildTree :: a -> (Closure, [Closure])
---buildTree x = (y, [mapM buildTree $ liftM allPtrs y])
---  where y = getBoxedClosureData x
 buildTree x = do
   cd <- getBoxedClosureData x
   return (cd, allPtrs cd)
 
 t x = unfoldTreeM buildTree x
+
+boxAddr :: Box -> Int
+boxAddr (Box any) = fromIntegral $ W# (aToWord# any)
+
+ins :: Box -> Closure -> IntMap.IntMap Closure -> IntMap.IntMap Closure
+ins box closure im = IntMap.insert (boxAddr box) closure im
+
+walkHeap a = go (asBox a)
+  where go b = do
+          c <- getBoxedClosureData b
+          ims <- liftM IntMap.unions (sequence (map go (allPtrs c)))
+          return $ (ins b c ims)
