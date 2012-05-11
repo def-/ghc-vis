626e3e6c39b7434c242a2f0c9de090eb46fadedd
r0q <deen@ti.elem>
2012-05-11 11:37:15 +0200

    more :print-like functionality

--- vis.hs
+++ vis.hs
@@ -7,6 +7,8 @@ import Control.Monad
 import Data.Tree
 import Data.Word
 import Data.Char
+import Data.List
+import Data.Maybe
 import GHC.Prim
 import GHC.Exts
 import Text.Printf
@@ -66,42 +68,75 @@ walkHeap a = go (asBox a) []
 --                      return $ (ins b c ims)
 
 dprint a = do h <- walkHeap a
-              case lookup (asBox a) h of
-                Nothing -> fail "value not found"
-                Just c  -> return $ tprint h c
+              return (tprint h (myLookup (asBox a) h))
 
-tprint h c@(ConsClosure (StgInfoTable _ _ CONSTR_0_1 _) _ [dataArg] descr) =
- case descr of
-    k | k `elem` [ "base:GHC.Word.W#"
+myLookup b h = fromJust $ lookup b h
+
+tprint h c@(ConsClosure (StgInfoTable _ _ CONSTR_0_1 _) _ [dataArg] _ modl name) =
+ case (modl, name) of
+    k | k `elem` [ ("GHC.Word", "W#")
+                 , ("GHC.Word", "W8#")
+                 , ("GHC.Word", "W16#")
+                 , ("GHC.Word", "W32#")
+                 , ("GHC.Word", "W64#")
+                 ] -> show dataArg
+
+    k | k `elem` [ ("GHC.Integer.Type", "S#")
+                 , ("GHC.Types", "I#")
+                 , ("GHC.Int", "I8#")
+                 , ("GHC.Int", "I16#")
+                 , ("GHC.Int", "I32#")
+                 , ("GHC.Int", "I64#")
                  ] -> show $ (fromIntegral :: Word -> Int) dataArg
 
-    k | k `elem` [ "integer-gmp:GHC.Integer.Type.S#"
-                 , "ghc-prim:GHC.Types.I#"
-                 , "ghc-prim:GHC.Types.I8#" -- TODO: Check those
-                 , "ghc-prim:GHC.Types.I16#"
-                 , "ghc-prim:GHC.Types.I32#"
-                 , "ghc-prim:GHC.Types.I64#"
-                 ] -> show $ (fromIntegral :: Word -> Int) dataArg
+    ("Types", "C#") -> show . chr $ fromIntegral dataArg
 
-    "ghc-prim:GHC.Types.C#" -> show . chr $ fromIntegral dataArg
+    ("Types", "D#") -> printf "%0.5f" (unsafeCoerce dataArg :: Double)
+    ("Types", "F#") -> printf "%0.5f" (unsafeCoerce dataArg :: Double)
 
-    "ghc-prim:GHC.Types.D#" -> printf "%0.5f" (unsafeCoerce dataArg :: Double)
-    "ghc-prim:GHC.Types.F#" -> printf "%0.5f" (unsafeCoerce dataArg :: Double)
+    c -> "Missing ConsClosure pattern for " ++ show c
 
-    c -> "Missing cons pattern for " ++ show c
+tprint h (ConsClosure (StgInfoTable 1 3 tipe 0) [bPtr] [_,start,end] _ "Data.ByteString.Internal" "PS")
+  = (printf "ByteString[%d,%d]" start end) ++ "(" ++ (tprint h cPtr) ++ ")"
+  where cPtr = myLookup bPtr h
 
-tprint h c@(ConsClosure (StgInfoTable _ _ CONSTR_NOCAF_STATIC _) _ _ "ghc-prim:GHC.Types.[]")
-  = "[]"
+tprint h (ConsClosure (StgInfoTable 2 3 CONSTR 1) [bPtr1,bPtr2] [_,start,end] _ "Data.ByteString.Lazy.Internal" "Chunk")
+  = (printf "Chunk[%d,%d]" start end) ++ "(" ++ (tprint h cPtr1) ++ " | " ++ (tprint h cPtr2) ++ ")"
+  where cPtr1 = myLookup bPtr1 h
+        cPtr2 = myLookup bPtr2 h
 
-tprint h c@(ConsClosure (StgInfoTable _ _ CONSTR_2_0 _) [bHead,bTail] _ "ghc-prim:GHC.Types.:")
+tprint h (ConsClosure (StgInfoTable 1 0 CONSTR_1_0 2) [bPtr] [] _ "GHC.ForeignPtr" "PlainPtr")
+  = tprint h cPtr
+  where cPtr = myLookup bPtr h
+
+tprint h (ConsClosure (StgInfoTable 1 0 CONSTR_1_0 1) [bPtr] [] _ "Data.Maybe" "Just")
+  = "Just " ++ tprint h cPtr
+  where cPtr = myLookup bPtr h
+
+tprint h (ConsClosure (StgInfoTable _ _ CONSTR_NOCAF_STATIC _) _ _ _ _ name)
+  = name
+
+tprint h (ConsClosure (StgInfoTable _ _ CONSTR_2_0 _) [bHead,bTail] _ _ "GHC.Types" ":")
   = tprint h cHead ++ ":" ++ tprint h cTail
-  where Just cHead = lookup bHead h
-        Just cTail = lookup bTail h
+  where cHead = myLookup bHead h
+        cTail = myLookup bTail h
 
-tprint h c@(IndClosure (StgInfoTable _ _ BLACKHOLE _) b)
+tprint h (ArrWordsClosure (StgInfoTable 0 0 ARR_WORDS 0) bytes arrWords)
+  = intercalate "," (map (printf "0x%x") arrWords :: [String])
+
+tprint h (IndClosure (StgInfoTable _ _ BLACKHOLE _) b)
   = tprint h c
-  where Just c = lookup b h
+  where c = myLookup b h
 
+tprint h (ThunkClosure (StgInfoTable 0 0 THUNK_STATIC 3) [] [])
+  = "Thunk"
+
+tprint h (ThunkClosure (StgInfoTable 1 1 THUNK_1_1 0) [bPtr] [arg])
+  = "Thunk(" ++ (tprint h cPtr) ++ ", " ++ show arg ++ ")"
+  where cPtr = myLookup bPtr h
+
+tprint h (FunClosure (StgInfoTable 0 1 tipe 0) [] [arg])
+  = "Fun(" ++ show arg ++ ")"
 
 tprint _ c = "Missing pattern for " ++ show c
 
