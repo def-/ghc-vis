64830f372d15b3ed1409818370306f2a436b7919
r0q <deen@cr.elem>
2012-05-14 13:10:37 +0200

    change to maps

--- vis.hs
+++ vis.hs
@@ -14,8 +14,11 @@ import GHC.Exts
 import Text.Printf
 import Unsafe.Coerce
 
-import Data.IntMap(IntMap)
-import qualified Data.IntMap as IntMap
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+--import Data.IntMap(IntMap)
+--import qualified Data.IntMap as IntMap
 
 -- Temporarily, probably use System.Mem.StableName
 instance Ord Box
@@ -27,9 +30,9 @@ instance Ord Box
 --        compare (UnnamedBox a) (UnnamedBox b) = compare a b
 --        compare (UnnamedBox a) (NamedBox _ b) = compare a b
 
-data NBox = NamedBox String Box
-          | UnnamedBox Box
-          deriving (Eq,Ord)
+--data NBox = NamedBox String Box
+--          | UnnamedBox Box
+--          deriving (Eq,Ord)
 
 buildTree x = do
   cd <- getBoxedClosureData x
@@ -57,19 +60,15 @@ isF _ = False
 --boxAddr :: Box -> Int
 --boxAddr (Box any) = fromIntegral $ W# (aToWord# any)
 
-ins :: [(Box, Closure)] -> Box -> Closure -> [(Box, Closure)]
---ins list box closure = (box, closure) : list
-ins list box closure = list ++ [(box, closure)]
-
 -- Run performGC from System.Mem first to get pointers not to change as much
 -- while executing
-walkHeap a = go (asBox a) []
-  where go b l = case lookup b l of
+walkHeap a = go (asBox a) Map.empty
+  where go b l = case Map.lookup b l of
                    Just _  -> return l
                    Nothing -> do
                      c' <- getBoxedClosureData b
                      l' <- foldM (\l x -> go x l) l (allPtrs c')
-                     return $ (ins l' b c')
+                     return $ (Map.insert b (Nothing, c') l')
 
 --walkHeapDepth a d = go (asBox a) d IntMap.empty
 --  where go b d im | d == 0 = return im
@@ -84,9 +83,10 @@ walkHeap a = go (asBox a) []
 dprint a = do h <- walkHeap a
               return (tprint h (myLookup (asBox a) h))
 
-myLookup b h = fromJust $ lookup b h
+myLookup b h = fromJust $ Map.lookup b h
 
-tprint h c@(ConsClosure (StgInfoTable _ _ CONSTR_0_1 _) _ [dataArg] _ modl name) =
+tprint :: Map Box (Maybe String, Closure) -> (Maybe String, Closure) -> String
+tprint h (_,(ConsClosure (StgInfoTable _ _ CONSTR_0_1 _) _ [dataArg] _ modl name)) =
  case (modl, name) of
     k | k `elem` [ ("GHC.Word", "W#")
                  , ("GHC.Word", "W8#")
@@ -110,63 +110,63 @@ tprint h c@(ConsClosure (StgInfoTable _ _ CONSTR_0_1 _) _ [dataArg] _ modl name)
 
     c -> "Missing ConsClosure pattern for " ++ show c
 
-tprint h (ConsClosure (StgInfoTable 1 3 CONSTR 0) [bPtr] [_,start,end] _ "Data.ByteString.Internal" "PS")
+tprint h (_,ConsClosure (StgInfoTable 1 3 CONSTR 0) [bPtr] [_,start,end] _ "Data.ByteString.Internal" "PS")
   = (printf "ByteString[%d,%d]" start end) ++ "(" ++ (tprint h cPtr) ++ ")"
   where cPtr = myLookup bPtr h
 
-tprint h (ConsClosure (StgInfoTable 2 3 CONSTR 1) [bPtr1,bPtr2] [_,start,end] _ "Data.ByteString.Lazy.Internal" "Chunk")
+tprint h (_,ConsClosure (StgInfoTable 2 3 CONSTR 1) [bPtr1,bPtr2] [_,start,end] _ "Data.ByteString.Lazy.Internal" "Chunk")
   = (printf "Chunk[%d,%d]" start end) ++ "(" ++ (tprint h cPtr1) ++ " | " ++ (tprint h cPtr2) ++ ")"
   where cPtr1 = myLookup bPtr1 h
         cPtr2 = myLookup bPtr2 h
 
-tprint h (ConsClosure (StgInfoTable 1 0 CONSTR_1_0 2) [bPtr] [] _ "GHC.ForeignPtr" "PlainPtr")
+tprint h (_,ConsClosure (StgInfoTable 1 0 CONSTR_1_0 2) [bPtr] [] _ "GHC.ForeignPtr" "PlainPtr")
   = tprint h cPtr
   where cPtr = myLookup bPtr h
 
-tprint h (ConsClosure (StgInfoTable 1 0 CONSTR_1_0 1) [bPtr] [] _ "Data.Maybe" "Just")
+tprint h (_,ConsClosure (StgInfoTable 1 0 CONSTR_1_0 1) [bPtr] [] _ "Data.Maybe" "Just")
   = "Just " ++ tprint h cPtr
   where cPtr = myLookup bPtr h
 
-tprint h (ConsClosure (StgInfoTable 0 0 CONSTR_NOCAF_STATIC 0) [] [] _ _ name)
+tprint h (_,ConsClosure (StgInfoTable 0 0 CONSTR_NOCAF_STATIC 0) [] [] _ _ name)
   = name
 
-tprint h (ConsClosure (StgInfoTable 0 _ CONSTR_NOCAF_STATIC 0) [] args _ _ name)
+tprint h (_,ConsClosure (StgInfoTable 0 _ CONSTR_NOCAF_STATIC 0) [] args _ _ name)
   = name ++ show args
 
-tprint h (ConsClosure (StgInfoTable 2 0 CONSTR_2_0 1) [bHead,bTail] [] _ "GHC.Types" ":")
+tprint h (_,ConsClosure (StgInfoTable 2 0 CONSTR_2_0 1) [bHead,bTail] [] _ "GHC.Types" ":")
   = tprint h cHead ++ ":" ++ tprint h cTail
   where cHead = myLookup bHead h
         cTail = myLookup bTail h
 
-tprint h (ArrWordsClosure (StgInfoTable 0 0 ARR_WORDS 0) bytes arrWords)
+tprint h (_,ArrWordsClosure (StgInfoTable 0 0 ARR_WORDS 0) bytes arrWords)
   = intercalate "," (map (printf "0x%x") arrWords :: [String])
 
-tprint h (IndClosure (StgInfoTable 1 0 BLACKHOLE 0) b)
+tprint h (_,IndClosure (StgInfoTable 1 0 BLACKHOLE 0) b)
   = tprint h c
   where c = myLookup b h
 
-tprint h (ThunkClosure (StgInfoTable 0 0 THUNK_STATIC 3) [] [])
+tprint h (_,ThunkClosure (StgInfoTable 0 0 THUNK_STATIC 3) [] [])
   = "Thunk"
 
-tprint h (ThunkClosure (StgInfoTable 1 1 THUNK_1_1 0) [bPtr] [arg])
+tprint h (_,ThunkClosure (StgInfoTable 1 1 THUNK_1_1 0) [bPtr] [arg])
   = "Thunk(" ++ (tprint h cPtr) ++ ", " ++ show arg ++ ")"
   where cPtr = myLookup bPtr h
 
-tprint h (ThunkClosure (StgInfoTable 2 0 THUNK_2_0 0) [bPtr1, bPtr2] [])
+tprint h (_,ThunkClosure (StgInfoTable 2 0 THUNK_2_0 0) [bPtr1, bPtr2] [])
   = "Thunk(" ++ (tprint h cPtr1) ++ ", " ++ (tprint h cPtr2) ++ ")"
   where cPtr1 = myLookup bPtr1 h
         cPtr2 = myLookup bPtr2 h
 
-tprint h (ThunkClosure (StgInfoTable 3 0 THUNK 0) [bPtr1, bPtr2, bPtr3] [])
+tprint h (_,ThunkClosure (StgInfoTable 3 0 THUNK 0) [bPtr1, bPtr2, bPtr3] [])
   = "Thunk(" ++ (tprint h cPtr1) ++ ", " ++ (tprint h cPtr2) ++ ", " ++ (tprint h cPtr3) ++ ")"
   where cPtr1 = myLookup bPtr1 h
         cPtr2 = myLookup bPtr2 h
         cPtr3 = myLookup bPtr3 h
 
-tprint h (FunClosure (StgInfoTable 0 1 tipe 0) [] [arg])
+tprint h (_,FunClosure (StgInfoTable 0 1 tipe 0) [] [arg])
   = "Fun(" ++ show arg ++ ")"
 
-tprint h (FunClosure (StgInfoTable 2 0 tipe 0) [bPtr1, bPtr2] [])
+tprint h (_,FunClosure (StgInfoTable 2 0 tipe 0) [bPtr1, bPtr2] [])
   = "Fun(" ++ (tprint h cPtr1) ++ ", " ++ (tprint h cPtr2) ++ ")"
   where cPtr1 = myLookup bPtr1 h
         cPtr2 = myLookup bPtr2 h
