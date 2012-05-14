90bd834c0f0a784bb3cfb661e27925ce984c1a18
r0q <deen@cr.elem>
2012-05-14 18:39:32 +0200

    names and evaluation

--- vis.hs
+++ vis.hs
@@ -88,17 +88,55 @@ walkHeap a = go (asBox a) Map.empty
 --                      ims <- foldM (\im x -> go x (d - 1) im) im (allPtrs c)
 --                      return $ (ins b c ims)
 
-dprint a = do h <- walkHeap a
-              return $ evalState (mprint (myLookup (asBox a) h)) (0,h)
+-- TODO: Proper forced evalution
+eval :: String -> HeapMap -> HeapMap
+eval name hm = Map.mapWithKey go hm
+  where go (Box a) (Just n, y) | n == name = seq a (Just n, y)
+                               | otherwise = (Just n, y)
+        go _ (x,y) = (x,y)
 
-myLookup b h = fromJust $ Map.lookup b h
+eval2 name (_,hm) = eval name hm
+
+bprint :: a -> IO String
+bprint a = do h <- walkHeap a
+              return $ evalState (mprint b (snd $ h Map.! b)) (0,h)
+  where b = asBox a
+
+dprint :: a -> IO (String, HeapMap)
+dprint a = do h <- walkHeap a
+              return $ evalState (do
+                s <- mprint b (snd $ h Map.! b)
+                (_,h) <- get
+                return (s,h)
+                ) (0,h)
+  where b = asBox a
 
 mlp :: Box -> PrintState String
 mlp b = do (_,h) <- get
-           mprint $ fromJust $ Map.lookup b h
+           mprint b (snd $ fromJust $ Map.lookup b h)
 
-mprint :: HeapEntry -> PrintState String
-mprint (_,ConsClosure (StgInfoTable _ _ CONSTR_0_1 _) _ [dataArg] _ modl name) =
+setName :: Box -> PrintState ()
+setName b = modify go
+  where go (i,h) = (i + 1, Map.adjust (set i) b h)
+        set i (Nothing, closure) = (Just ("t" ++ show i), closure)
+
+getName :: Box -> PrintState (Maybe String)
+getName b = do (_,h) <- get
+               return $ fst $ fromJust $ Map.lookup b h
+
+getSetName :: Box -> PrintState String
+getSetName b = do mn <- getName b
+                  case mn of
+                    Nothing   -> do setName b
+                                    n <- getName b
+                                    return $ fromJust n
+                    Just name -> return name
+
+-- TODO: Make examples.hs
+-- TODO: Generalize mprint
+
+mprint :: Box -> Closure -> PrintState String
+mprint _ (ConsClosure (StgInfoTable _ _ _ _) _ [dataArg] _ modl name) =
  return $ case (modl, name) of
     k | k `elem` [ ("GHC.Word", "W#")
                  , ("GHC.Word", "W8#")
@@ -122,65 +160,81 @@ mprint (_,ConsClosure (StgInfoTable _ _ CONSTR_0_1 _) _ [dataArg] _ modl name) =
 
     c -> "Missing ConsClosure pattern for " ++ show c
 
-mprint (_,ConsClosure (StgInfoTable 1 3 CONSTR 0) [bPtr] [_,start,end] _ "Data.ByteString.Internal" "PS")
+mprint _ (ConsClosure (StgInfoTable 1 3 CONSTR 0) [bPtr] [_,start,end] _ "Data.ByteString.Internal" "PS")
   = do cPtr  <- mlp bPtr
        return $ printf "ByteString[%d,%d](%s)" start end cPtr
 
-mprint (_,ConsClosure (StgInfoTable 2 3 CONSTR 1) [bPtr1,bPtr2] [_,start,end] _ "Data.ByteString.Lazy.Internal" "Chunk")
+mprint _ (ConsClosure (StgInfoTable 2 3 CONSTR 1) [bPtr1,bPtr2] [_,start,end] _ "Data.ByteString.Lazy.Internal" "Chunk")
   = do cPtr1 <- mlp bPtr1
        cPtr2 <- mlp bPtr2
        return $ printf "Chunk[%d,%d](%s|%s)" start end cPtr1 cPtr2
 
-mprint (_,ConsClosure (StgInfoTable 1 0 CONSTR_1_0 2) [bPtr] [] _ "GHC.ForeignPtr" "PlainPtr")
-  = mlp bPtr
+--mprint _ (ConsClosure (StgInfoTable 1 0 CONSTR_1_0 2) [bPtr] [] _ "GHC.ForeignPtr" "PlainPtr")
+--  = mlp bPtr
 
-mprint (_,ConsClosure (StgInfoTable 1 0 CONSTR_1_0 1) [bPtr] [] _ _ name)
+mprint _ (ConsClosure (StgInfoTable 1 0 CONSTR_1_0 _) [bPtr] [] _ _ name)
   = do cPtr <- mlp bPtr
-       return $ name ++ cPtr
+       return $ printf "%s (%s)" name cPtr
 
-mprint (_,ConsClosure (StgInfoTable 0 0 CONSTR_NOCAF_STATIC 0) [] [] _ _ name)
+mprint _ (ConsClosure (StgInfoTable 0 0 CONSTR_NOCAF_STATIC _) [] [] _ _ name)
   = return name
 
-mprint (_,ConsClosure (StgInfoTable 0 _ CONSTR_NOCAF_STATIC 0) [] args _ _ name)
-  = return $ name ++ show args
+mprint _ (ConsClosure (StgInfoTable 0 _ CONSTR_NOCAF_STATIC _) [] args _ _ name)
+  = return $ printf "%s (%s)" name (show args)
 
-mprint (_,ConsClosure (StgInfoTable 2 0 CONSTR_2_0 1) [bHead,bTail] [] _ "GHC.Types" ":")
+mprint _ (ConsClosure (StgInfoTable 2 0 CONSTR_2_0 1) [bHead,bTail] [] _ "GHC.Types" ":")
   = do cHead <- mlp bHead
        cTail <- mlp bTail
-       return $ cHead ++ ":" ++ cTail
+       return $ printf "%s : %s" cHead cTail
 
-mprint (_,ArrWordsClosure (StgInfoTable 0 0 ARR_WORDS 0) bytes arrWords)
+mprint _ (ArrWordsClosure (StgInfoTable 0 0 ARR_WORDS 0) bytes arrWords)
   = return $ intercalate "," (map (printf "0x%x") arrWords :: [String])
 
-mprint (_,IndClosure (StgInfoTable 1 0 BLACKHOLE 0) b)
+mprint _ (IndClosure (StgInfoTable 1 0 BLACKHOLE 0) b)
   = mlp b
 
-mprint (_,ThunkClosure (StgInfoTable 0 0 THUNK_STATIC 3) [] [])
-  = return "Thunk"
+mprint b (ThunkClosure (StgInfoTable 0 0 THUNK_STATIC 3) [] [])
+  = getSetName b
 
-mprint (_,ThunkClosure (StgInfoTable 1 1 THUNK_1_1 0) [bPtr] [arg])
-  = do cPtr <- mlp bPtr
-       return $ printf "Thunk(%s, %s)" cPtr (show arg)
+mprint b (ThunkClosure (StgInfoTable 1 1 THUNK_1_1 0) [bPtr] [arg])
+  = do name <- getSetName b
+       cPtr <- mlp bPtr
+       return $ printf "%s(%s, %s)" name cPtr (show arg)
 
-mprint (_,ThunkClosure (StgInfoTable 2 0 THUNK_2_0 0) [bPtr1, bPtr2] [])
-  = do cPtr1 <- mlp bPtr1
-       cPtr2 <- mlp bPtr2
-       return $ printf "Thunk(%s, %s)" cPtr1 cPtr2
+-- TODO: Reverse order of ptrs?
+mprint b (ThunkClosure (StgInfoTable _ _ _ _) bPtrs args)
+  = do name <- getSetName b
+       cPtrs <- mapM mlp bPtrs
+       let cArgs = map show args
+           tPtrs = intercalate ", " cPtrs
+           tArgs = intercalate ", " cArgs
+       return $ printf "%s(%s | %s)" name tPtrs tArgs
 
-mprint (_,ThunkClosure (StgInfoTable 3 0 THUNK 0) [bPtr1, bPtr2, bPtr3] [])
-  = do cPtr1 <- mlp bPtr1
-       cPtr2 <- mlp bPtr2
-       cPtr3 <- mlp bPtr3
-       return $ printf "Thunk(%s, %s, %s)" cPtr1 cPtr2 cPtr3
+--mprint b (ThunkClosure (StgInfoTable 3 0 THUNK 0) [bPtr1, bPtr2, bPtr3] [])
+--  = do name <- getSetName b
+--       cPtr1 <- mlp bPtr1
+--       cPtr2 <- mlp bPtr2
+--       cPtr3 <- mlp bPtr3
+--       return $ printf "%s(%s, %s, %s)" name cPtr1 cPtr2 cPtr3
 
-mprint (_,FunClosure (StgInfoTable 0 1 tipe 0) [] [arg])
-  = return $ "Fun(" ++ show arg ++ ")"
+--mprint _ (FunClosure (StgInfoTable 0 1 tipe 0) [] [arg])
+--  = return $ printf "Fun(%s)" (show arg)
+--
+--mprint _ (FunClosure (StgInfoTable 2 0 tipe 0) [bPtr1, bPtr2] [])
+--  = do cPtr1 <- mlp bPtr1
+--       cPtr2 <- mlp bPtr2
+--       return $ printf "Fun(%s, %s)" cPtr1 cPtr2
 
-mprint (_,FunClosure (StgInfoTable 2 0 tipe 0) [bPtr1, bPtr2] [])
-  = do cPtr1 <- mlp bPtr1
-       cPtr2 <- mlp bPtr2
-       return $ printf "Fun(%s, %s)" cPtr1 cPtr2
+mprint _ (FunClosure (StgInfoTable _ _ _ _) bPtrs args)
+  = do cPtrs <- mapM mlp bPtrs
+       let cArgs = map show args
+           tPtrs = intercalate ", " cPtrs
+           tArgs = intercalate ", " cArgs
+       return $ printf "Fun(%s | %s)" tPtrs tArgs
 
-mprint c = return $ "Missing pattern for " ++ show c
+mprint b (APClosure (StgInfoTable 0 0 _ _) _ _ _ _)
+  = getSetName b
+
+mprint _ c = return $ "Missing pattern for " ++ show c
 
 tseq (Box a) = seq a ()
