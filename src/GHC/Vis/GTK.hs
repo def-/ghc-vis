af06402766ce8b65a707010fb90d035f41cbdb22
r0q <deen@ti.elem>
2012-06-20 16:02:58 +0200

    Using MVar, working wakeup

--- src/GHC/Vis/GTK.hs
+++ src/GHC/Vis/GTK.hs
@@ -7,16 +7,15 @@ import Graphics.UI.Gtk
 import Graphics.Rendering.Cairo
 
 import Control.Concurrent
-
-import Data.IORef
+import Control.Concurrent.MVar
 
 import GHC.Vis
 import GHC.HeapView
 
-tvis ref = do
-  forkIO $ vis ref
+tvis ref bref = do
+  forkIO $ vis ref bref
 
-vis ref = do
+vis ref bref = do
   initGUI
   window <- windowNew
   onDestroy window mainQuit
@@ -32,17 +31,25 @@ vis ref = do
     return True
 
   widgetShowAll window
+
+  forkIO $ react bref window
+
   mainGUI
 
+react bref window = do
+  takeMVar bref
+  widgetQueueDraw window
+  react bref window
+
 redraw canvas ref = do
-  boxes <- readIORef ref
+  boxes <- readMVar ref
   texts <- mapM (\(Box a) -> bprint a) boxes
-  render canvas ref $ do
+  render canvas $ do
     setSourceRGB 0 0 0
     moveTo 10 10
     showText $ show texts
 
-render canvas ref r = do
+render canvas r = do
         win <- widgetGetDrawWindow canvas
         renderWithDrawable win $ do
           r
