104d42c51633511ba4a4c4150867863db72a6b76
r0q <deen@ti.elem>
2012-06-21 03:00:27 +0200

    start some graph stuff

--- src/GHC/Vis.hs
+++ src/GHC/Vis.hs
@@ -2,9 +2,14 @@
 
 module GHC.Vis (
   walkHeap,
+  mWalkHeap,
   dprint,
   bprint,
   mprint,
+  parseClosure,
+  VisObject(..),
+  getID,
+  buildGraph,
   eval,
   evalS,
   evalP,
@@ -21,6 +26,7 @@ import Control.DeepSeq
 import Control.Monad
 import Control.Monad.State
 import Data.Tree
+import Data.Graph.Inductive
 import Data.Word
 import Data.Char
 import Data.List
@@ -56,6 +62,24 @@ instance Ord Box
           return $ compare (hashStableName a) (hashStableName b)
           )
 
+data VisObject = Unnamed String
+               | Named String [VisObject]
+               | Link String
+
+getID a = makeStableName a >>= return . hashStableName
+
+buildGraph :: [Box] -> IO (Gr Closure ())
+buildGraph boxes = foldM go empty boxes
+  where go graph box@(Box content) = do
+          closure  <- getBoxedClosureData box
+          nodename <- getID content
+          edges <- mapM (\(Box x) -> getID x >>= \target -> return (nodename, target, ())) (nearlyAllPtrs closure)
+          if nodename `gelem` graph
+            then return graph
+            else do
+                  graph' <- foldM go (insNode (nodename, closure) graph) $ nearlyAllPtrs closure 
+                  return $ insEdges edges graph'
+
 buildTree x = do
   cd <- getBoxedClosureData x
   return (cd, allPtrs cd)
@@ -84,10 +108,21 @@ isF _ = False
 
 -- Don't inspect deep pointers in BCOClosures for now, they never end
 -- TODO: What we could do is output the instrs and literals of it
---nearlyAllPtrs (BCOClosure (StgInfoTable _ _ _ _) i l _b _ _ _) = [i,l]
+nearlyAllPtrs (BCOClosure (StgInfoTable _ _ _ _) i l _b _ _ _) = []
 --nearlyAllPtrs x@(BCOClosure _ _ _ _ _ _ _) = filter notBreakInfo $ allPtrs x
 --  where notBreakInfo =
 nearlyAllPtrs x = allPtrs x
+
+mWalkHeap :: [a] -> IO HeapMap
+mWalkHeap as = do
+  performGC
+  foldM (\l a -> go (asBox a) l) Map.empty as
+  where go b l = case Map.lookup b l of
+                   Just _  -> return l
+                   Nothing -> do
+                     c' <- getBoxedClosureData b
+                     l' <- foldM (\l x -> go x l) (Map.insert b (Nothing, c') l) (nearlyAllPtrs c')
+                     return $ (Map.insert b (Nothing, c') l')
 
 -- Run performGC from System.Mem first to get pointers not to change as much
 -- while executing
@@ -338,6 +373,148 @@ mprint b (PAPClosure (StgInfoTable 0 0 _ _) _ _ _ _)
 
 
 mprint _ c = return $ "Missing pattern for " ++ show c
+
+parseClosure :: Box -> Closure -> PrintState VisObject
+parseClosure _ (ConsClosure (StgInfoTable _ _ _ _) _ [dataArg] _ modl name) =
+ return $ Unnamed $ case (modl, name) of
+    k | k `elem` [ ("GHC.Word", "W#")
+                 , ("GHC.Word", "W8#")
+                 , ("GHC.Word", "W16#")
+                 , ("GHC.Word", "W32#")
+                 , ("GHC.Word", "W64#")
+                 ] -> show dataArg
+
+    k | k `elem` [ ("GHC.Integer.Type", "S#")
+                 , ("GHC.Types", "I#")
+                 , ("GHC.Int", "I8#")
+                 , ("GHC.Int", "I16#")
+                 , ("GHC.Int", "I32#")
+                 , ("GHC.Int", "I64#")
+                 ] -> show $ (fromIntegral :: Word -> Int) dataArg
+
+    ("GHC.Types", "C#") -> show . chr $ fromIntegral dataArg
+
+    ("Types", "D#") -> printf "%0.5f" (unsafeCoerce dataArg :: Double)
+    ("Types", "F#") -> printf "%0.5f" (unsafeCoerce dataArg :: Double)
+
+    c -> "Missing ConsClosure pattern for " ++ show c
+
+-- Empty ByteStrings point to a nullForeignPtr, evaluating it leads to an
+-- Prelude.undefined exception
+--parseClosure _ (ConsClosure (StgInfoTable 1 3 _ 0) _ [_,0,0] _ "Data.ByteString.Internal" "PS")
+--  = return "ByteString[0,0]()"
+--
+--parseClosure _ (ConsClosure (StgInfoTable 1 3 _ 0) [bPtr] [_,start,end] _ "Data.ByteString.Internal" "PS")
+--  = do cPtr  <- mlp bPtr
+--       return $ printf "ByteString[%d,%d](%s)" start end cPtr
+--
+--parseClosure _ (ConsClosure (StgInfoTable 2 3 _ 1) [bPtr1,bPtr2] [_,start,end] _ "Data.ByteString.Lazy.Internal" "Chunk")
+--  = do cPtr1 <- mlp bPtr1
+--       cPtr2 <- mlp bPtr2
+--       return $ printf "Chunk[%d,%d](%s,%s)" start end cPtr1 cPtr2
+--
+--parseClosure _ (ConsClosure (StgInfoTable 1 0 CONSTR_1_0 _) [bPtr] [] _ _ name)
+--  = do cPtr <- mlp bPtr
+--       return $ name ++ " " ++ mbParens cPtr
+--
+--parseClosure _ (ConsClosure (StgInfoTable 0 0 CONSTR_NOCAF_STATIC _) [] [] _ _ name)
+--  = return name
+--
+--parseClosure _ (ConsClosure (StgInfoTable 0 _ CONSTR_NOCAF_STATIC _) [] args _ _ name)
+--  = return $ name ++ " " ++ show args
+--
+--parseClosure b (ConsClosure (StgInfoTable 2 0 _ 1) [bHead,bTail] [] _ "GHC.Types" ":")
+--  = do b' <- getName b
+--       case b' of
+--         Just n  -> return n
+--         Nothing -> do i <- getCount
+--                       setName b
+--                       n <- liftM (fromJust2 "4") $ getName b
+--                       cHead <- liftM mbParens $ mlp bHead
+--                       cTail <- liftM mbParens $ mlp bTail
+--                       r <- countReferences b
+--                       return $ case r > 1 || (i == 0 && r == 1) of
+--                         True -> printf "%s=(%s:%s)" n cHead cTail
+--                         False -> printf "%s:%s" cHead cTail
+--
+--parseClosure b (ConsClosure (StgInfoTable _ 0 _ _) bPtrs [] _ _ name)
+--  = do b' <- getName b
+--       case b' of
+--         Just n  -> return n
+--         Nothing -> do i <- getCount
+--                       setName b
+--                       n <- liftM (fromJust2 "5") $ getName b
+--                       cPtrs <- mapM ((liftM mbParens) . mlp) bPtrs
+--                       let tPtrs = intercalate " " cPtrs
+--                       r <- countReferences b
+--                       return $ case r > 1 || (i == 0 && r == 1) of
+--                         True -> printf "%s=(%s %s)" n name tPtrs
+--                         False -> printf "%s %s" name tPtrs
+--
+--
+--parseClosure _ (ArrWordsClosure (StgInfoTable 0 0 ARR_WORDS 0) bytes arrWords)
+--  = return $ intercalate "," (map (printf "0x%x") arrWords :: [String])
+--
+--parseClosure _ (IndClosure (StgInfoTable 1 0 _ 0) b)
+--  = mlp b
+--
+---- Reversed order of ptrs
+--parseClosure b (ThunkClosure (StgInfoTable _ _ _ _) bPtrs args)
+--  = do name <- getSetName b
+--       cPtrs <- mapM mlp $ reverse bPtrs
+--       let tPtrs = intercalate "," cPtrs
+--       return $ case null args of
+--         True  -> name ++ "(" ++ tPtrs ++ ")"
+--         False -> name ++ show args ++ "(" ++ tPtrs ++ ")"
+--
+--parseClosure b (FunClosure (StgInfoTable _ _ _ _) bPtrs args)
+--  = do name <- getSetName b
+--       cPtrs <- mapM mlp $ reverse bPtrs
+--       let tPtrs = intercalate "," cPtrs
+--       return $ case null args of
+--         True  -> name ++ "(" ++ tPtrs ++ ")"
+--         False -> name ++ show args ++ "(" ++ tPtrs ++ ")"
+--
+--parseClosure b (APClosure (StgInfoTable 0 0 _ _) _ _ _ _)
+--  = getSetName b
+--
+---- λ> let f x = 3 * x
+---- λ> let a = map f [1..3]
+---- λ> printP a
+---- t0
+---- λ> evalP a "t0"
+---- t1(1,t2):t3(t4(t5(3,1),1,1),t2)
+---- λ> evalP a "t3"
+---- t1(1,t2):t4(2,t2):t5(t6(t7(3,1),2,1),t2)
+---- λ> evalP a "t5"
+---- t1(1,t2):t4(2,t2):t6(3,t2):t7(t8(t9(3,1),3,1),t2)
+---- λ> evalP a "t6"
+---- (t1(1,Missing pattern for PAPClosure {info = StgInfoTable {ptrs = 0, nptrs = 0, tipe = PAP, srtlen = 0}, arity = 2526134728, n_args = 32539, fun = 0x00007f1b9691c1c8, payload = [0x0000000041a6edf0]})):*** Exception: fromJust2
+--
+--parseClosure b (PAPClosure (StgInfoTable 0 0 _ _) _ _ _ _)
+--  = getSetName b
+--
+---- λ> let f x = 3 * x
+---- λ> let a = map f [1..3]
+---- λ> printP a
+---- t0
+---- λ> evalP a "t0"
+---- t1(1,t2):t3(t4(t5(3,1),1,1),t2)
+---- λ> evalP a "t3"
+---- t1(1,t2):t4(2,t2):t5(t6(t7(3,1),2,1),t2)
+---- λ> evalP a "t5"
+---- t1(1,t2):t4(2,t2):t6(3,t2):t7(t8(t9(3,1),3,1),t2)
+---- λ> evalP a "t6"
+---- *** Exception: fromJust1
+--
+---- λ> let h = let y = id (:) () y in h
+---- λ> h
+---- ^CInterrupted.
+---- λ> walkHeap h
+---- *** Exception: getClosureData: Cannot handle closure type AP_STACK
+--
+--
+--parseClosure _ c = return $ "Missing pattern for " ++ show c
 
 tseq (Box a) = seq a ()
 
--- src/GHC/Vis/GTK.hs
+++ src/GHC/Vis/GTK.hs
@@ -11,10 +11,14 @@ import Graphics.Rendering.Cairo
 import Control.Concurrent
 import Control.Concurrent.MVar
 
+import Control.Monad
+
+import System.IO.Unsafe
+
 import GHC.Vis
 import GHC.HeapView
 
-import System.IO.Unsafe
+fontSize = 15
 
 visGlobalVar = unsafePerformIO (Control.Concurrent.MVar.newEmptyMVar :: IO (Control.Concurrent.MVar.MVar GHC.Vis.GTK.Signal))
 
@@ -53,7 +57,7 @@ react ref bref canvas = do
   case signal of
     NewSignal x  -> modifyMVar_ bref (\y -> return $ y ++ [x])
     ClearSignal  -> modifyMVar_ bref (\_ -> return [])
-    UpdateSignal -> return ()
+    UpdateSignal -> putStrLn "update" >> return ()
 
   threadDelay 10000 -- 10 ms, else sometimes redraw happens too fast (Why?)
   widgetQueueDraw canvas
@@ -61,13 +65,94 @@ react ref bref canvas = do
 
 redraw canvas bref = do
   boxes <- readMVar bref
-  texts <- mapM (\(Box a) -> bprint a) boxes
+  heapMap <- mWalkHeap boxes
+  --texts <- mapM (\(Box a) -> bprint a) boxes
   render canvas $ do
-    setSourceRGB 0 0 0
-    moveTo 10 10
-    showText $ show texts
+    mapM (drawEntry heapMap) boxes
+    --setSourceRGB 0 0 0
+    --moveTo 10 10
+    --showText $ show texts
 
 render canvas r = do
         win <- widgetGetDrawWindow canvas
         renderWithDrawable win $ do
+          selectFontFace "DejaVu Sans" FontSlantNormal FontWeightNormal
+          setFontSize fontSize
           r
+
+drawEntry heapMap box = do
+  let text = show box
+  TextExtents xb yb w h _ _ <- textExtents text
+  translate 100 30
+  setSourceRGB 0 0 0
+  moveTo (-w/2) (h/2)
+  showText text
+
+drawLinkObject heapMap box = do
+  let text = "t1"
+  let padding = 5
+  TextExtents xb yb w h _ _ <- textExtents text
+
+  translate 100 30
+
+  let (ux, uy, uw, uh) =
+        ( -w/2 + xb - padding
+        ,  h/2 + yb - padding
+        ,  w   +  2 * padding
+        ,  h   +  2 * padding
+        )
+
+  setLineCap LineCapRound
+  roundedRect ux uy uw uh
+  setSourceRGB 1 0 0
+  fillPreserve
+  setSourceRGB 0 0 0
+  stroke
+
+  moveTo (-w/2) (h/2)
+  showText text
+
+drawNamedObject heapMap box = do
+  let text = show box
+  let text1 = "t1"
+  let padding = 5
+  TextExtents xb yb w h _ _ <- textExtents text
+  TextExtents xb1 yb1 w1 h1 _ _ <- textExtents text1
+
+  translate 100 30
+
+  let (ux, uy, uw, uh) =
+        ( -(max w w1)/2 + xb - 1.5 * padding
+        ,  (h + h1)/2   + yb - 1.5 * padding
+        ,  (max w w1)   +  3 * padding
+        ,  (h + h1)     +  3 * padding
+        )
+
+  --setLineWidth 10
+  setLineCap LineCapRound
+  roundedRect ux uy uw uh
+  setSourceRGB 1 0 0
+  fillPreserve
+  setSourceRGB 0 0 0
+  stroke
+  moveTo ux (h + padding/2)
+  lineTo (ux + uw) (h + padding/2)
+  stroke
+  moveTo (-w/2) (h/2 + padding)
+  showText text
+  moveTo (-w1/2) (h + h1/2 + 2 * padding)
+  showText text1
+
+roundedRect x y w h = do
+  moveTo       x            (y+pad)
+  lineTo       x            (y + h - pad)
+  arcNegative (x + pad)     (y + h - pad) pad pi (pi/2)
+  lineTo      (x + w - pad) (y + h)
+  arcNegative (x + w - pad) (y + h - pad) pad (pi/2) 0
+  lineTo      (x + w)       (y + pad)
+  arcNegative (x + w - pad) (y + pad)     pad 0  (-pi/2)
+  lineTo      (x + pad)      y
+  arcNegative (x + pad)     (y + pad)     pad (-pi/2) (-pi)
+  closePath
+
+  where pad = 1/10 * min w h
