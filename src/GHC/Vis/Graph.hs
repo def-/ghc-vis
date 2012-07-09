38e50a222d7753e0dbe6b2cce17197a555750a04
r0q <deen@ti.elem>
2012-07-09 03:40:13 +0200

    Start xdot parsing

--- src/GHC/Vis/Graph.hs
+++ src/GHC/Vis/Graph.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 --module GHC.Vis.Graph (
 --)
 --where
@@ -5,13 +7,19 @@
 import Control.Monad
 import Data.Tuple
 import Data.Maybe
+import Data.Char
+import Data.Ratio
 import Data.Text.IO
 import qualified Data.Text.Lazy as B
+import qualified Data.Text.Lazy.Read as B
+
+import qualified Text.ParserCombinators.Poly.StateText as P
 
 import Data.Graph.Inductive
 
-import Data.GraphViz
+import Data.GraphViz hiding (Ellipse)
 import Data.GraphViz.Types
+import Data.GraphViz.Parsing
 import qualified Data.GraphViz.Types.Generalised as G
 
 import GHC.HeapView
@@ -116,3 +124,85 @@ dg as = do
   hm <- walkHeapWithoutDummy as
   xDotText <- graphvizWithHandle Dot (defaultVis $ toViewableGraph $ buildGraph hm) XDot hGetContents
   return $ parseDotGraph $ B.fromChunks [xDotText]
+
+type Point = (Double, Double)
+
+data Alignment = LeftAlign
+               | CenterAlign
+               | RightAlign
+               deriving Show
+
+-- http://www.graphviz.org/doc/info/output.html#d:xdot
+data Operation = Ellipse { xy :: Point, w :: Double, h :: Double, filled :: Bool }
+               | Polygon { points :: [Point], filled :: Bool }
+               | BSpline { points :: [Point], filled :: Bool }
+               | Text { baseline :: Point, alignment :: Alignment, width :: Double, text :: String }
+               | Color { color :: String, filled :: Bool }
+               | Font { size :: Double, name :: String }
+               | Style { style :: String }
+               | Image { xy :: Point, w :: Double, h :: Double, name :: String }
+               deriving Show
+
+parse = P.runParser p [] str3
+  where str1 = B.pack "c 9 -#000000ff e 63 167 27 19 "
+        str2 = B.pack "F 14.000000 11 -Times-Roman c 9 -#000000ff T 63 163 0 4 1 -: "
+        str3 = B.pack "e 1.00000 2.3 2 4"
+        p = do
+          t <- P.next
+          s <- P.next
+          r <- case s of
+            ' ' -> case t of
+                     'E' -> parseEllipse True
+                     'e' -> parseEllipse False
+                     _   -> fail "Unknown Operation"
+            _   -> fail "Space missing"
+          P.next
+          return r
+        parseEllipse filled = do
+          x <- P.manySatisfy (/= ' ')
+          P.next
+          y <- P.manySatisfy (/= ' ')
+          P.next
+          w <- P.manySatisfy (/= ' ')
+          P.next
+          h <- P.manySatisfy (/= ' ')
+          return $ Ellipse (0,0) 0 0 filled
+
+runParser' p = checkValidParse . fst . P.runParser p'
+  where
+    p' = p `discard` (whitespace >> P.eof)
+
+parseSigned p = (character '-' >> liftM negate p)
+                `P.onFail`
+                p
+
+parseInt = do cs <- P.many1Satisfy isDigit
+              case B.decimal cs of
+                Right (n,"")  -> return n
+                Right (_,txt) -> fail $ "Trailing digits not parsed as Integral: " ++ B.unpack txt
+                Left err      -> fail $ "Could not read Integral: " ++ err
+           `P.adjustErr` ("Expected one or more digits\n\t"++)
+
+parseInt' = parseSigned parseInt
+
+parseFloat = do ds   <- P.manySatisfy isDigit
+                frac <- P.optional
+                        $ do character '.'
+                             P.manySatisfy isDigit
+                when (B.null ds && noDec frac)
+                  (fail "No actual digits in floating point number!")
+                expn  <- P.optional parseExp
+                when (isNothing frac && isNothing expn)
+                  (fail "This is an integer, not a floating point number!")
+                let frac' = fromMaybe "" frac
+                    expn' = fromMaybe 0 expn
+                ( return . fromRational . (* (10^^(expn' - fromIntegral (B.length frac'))))
+                  . (%1) . P.runParser parseInt) (ds `B.append` frac')
+             `P.onFail`
+             fail "Expected a floating point number"
+  where
+    parseExp = do character 'e'
+                  ((character '+' >> parseInt)
+                   `P.onFail`
+                   parseInt')
+    noDec = maybe True B.null
