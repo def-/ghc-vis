8fd30c53e5d9129a7a9b6b2cfd768e2b9374bc5b
r0q <deen@cr.elem>
2012-09-01 03:32:30 +0200

    Add post install message to setup.hs

--- Setup.hs
+++ Setup.hs
@@ -1,3 +1,17 @@
 module Main where
+
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.Setup
+import Distribution.Simple.LocalBuildInfo
+
+import System.FilePath
+
+main = defaultMainWithHooks $
+       simpleUserHooks { postInst = postInstHook (postInst simpleUserHooks) }
+
+postInstHook oldHook args iflags pDesc lbi = do
+  let instDataDir = datadir $ absoluteInstallDirs pDesc lbi (fromFlag $ copyDest defaultCopyFlags)
+  putStrLn "To use ghc-vis you have to load its ghci file in GHCi. To do this automatically when GHCi is started run:"
+  putStrLn $ "echo \":script " ++ (instDataDir </> "ghci") ++ "\" >> ~/.ghci"
+
+  oldHook args iflags pDesc lbi
