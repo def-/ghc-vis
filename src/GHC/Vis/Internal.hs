e0e22335dbacbaa00f2c3279501c0d1a7be1bb6f
def <dennis@felsin9.de>
2012-10-18 21:13:34 +0200

    ghc-heap-view 0.3.0.3

--- src/GHC/Vis/Internal.hs
+++ src/GHC/Vis/Internal.hs
@@ -392,13 +392,13 @@ parseInternal b (PAPClosure _ _ _ fun pl) = do
       sPtrs = if null tPtrs then [Unnamed ""] else Unnamed "[" : tPtrs ++ [Unnamed "]"]
   return $ Function (infixFix name) : fPtr ++ sPtrs
 
---parseInternal b (APStackClosure _ fun pl) = do
---  name <- getSetName b
---  fPtr <- contParse fun
---  pPtrs <- mapM contParse $ reverse pl
---  let tPtrs = intercalate [Unnamed ","] pPtrs
---      sPtrs = if null tPtrs then [Unnamed ""] else Unnamed "[" : tPtrs ++ [Unnamed "]"]
---  return $ Thunk (infixFix name) : fPtr ++ sPtrs
+parseInternal b (APStackClosure _ fun pl) = do
+  name <- getSetName b
+  fPtr <- contParse fun
+  pPtrs <- mapM contParse $ reverse pl
+  let tPtrs = intercalate [Unnamed ","] pPtrs
+      sPtrs = if null tPtrs then [Unnamed ""] else Unnamed "[" : tPtrs ++ [Unnamed "]"]
+  return $ Thunk (infixFix name) : fPtr ++ sPtrs
 
 parseInternal _ (MVarClosure _ qHead qTail qValue)
    = do cHead <- liftM mbParens $ contParse qHead
@@ -523,8 +523,8 @@ showClosure APClosure{}
 showClosure PAPClosure{}
   = "PAP"
 
---showClosure APStackClosure{}
---  = "APStack"
+showClosure APStackClosure{}
+  = "APStack"
 
 showClosure BCOClosure{}
   = "BCO"
