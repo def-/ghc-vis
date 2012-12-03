7c790e0e5d86dbbd33ac185169a3082ddfc6b599
def <dennis@felsin9.de>
2012-12-03 18:09:12 +0100

    Throw an error when trying to compile using profiling

--- Setup.hs
+++ Setup.hs
@@ -3,11 +3,14 @@ module Main where
 import Distribution.Simple
 import Distribution.Simple.Setup
 import Distribution.Simple.LocalBuildInfo
+import Distribution.PackageDescription (PackageDescription)
 
 import System.FilePath
 
-main = defaultMainWithHooks $
-       simpleUserHooks { postInst = postInstHook (postInst simpleUserHooks) }
+main = defaultMainWithHooks $ simpleUserHooks
+  { postInst = postInstHook (postInst simpleUserHooks)
+  , postConf = postConfHook (postConf simpleUserHooks)
+  }
 
 postInstHook oldHook args iflags pDesc lbi = do
   let instDataDir = datadir $ absoluteInstallDirs pDesc lbi (fromFlag $ copyDest defaultCopyFlags)
@@ -15,3 +18,8 @@ postInstHook oldHook args iflags pDesc lbi = do
   putStrLn $ "echo \":script " ++ (instDataDir </> "ghci") ++ "\" >> ~/.ghci"
 
   oldHook args iflags pDesc lbi
+
+postConfHook oldHook args flags descr buildInfo = case profFlag of
+  Flag True -> error "This library cannot be built using profiling. Try invoking cabal with the --disable-library-profiling flag."
+  _ -> oldHook args flags descr buildInfo
+  where profFlag = configProfLib $ configFlags buildInfo
