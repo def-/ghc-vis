3f1d2a9d26812bf75fe1747ccdffe86b92008f11
def <dennis@felsin9.de>
2013-06-07 08:43:45 +0200

    All to doc

---
+++ doc/create.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+ghci -ghci-script create.ghci
+
+for i in *svg; do
+  inkscape -z -e ${i:r}.png -d 60 -b white $i
+
+  #gzip -9 $i
+  #mv $i.gz ${i:r}.svgz
+
+  pngnq ${i:r}.png
+  optipng -o7 ${i:r}-nq8.png
+  mv ${i:r}-nq8.png ${i:r}.png
+done
