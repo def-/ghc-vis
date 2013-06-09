8072f9b3d360b2f928527fbd80eca902b9e1cb78
def <dennis@felsin9.de>
2013-06-09 12:10:20 +0200

    Gentoo update

--- docs/Makefile.sh
+++ docs/Makefile.sh
@@ -16,5 +16,5 @@ for i in *svg; do
 done
 
 for i in **/*.txt; do
-  asciidoc -b xhtml11 -f asciidoc.conf -a disable-javascript -a data-uri -a stylesheet=$PWD/asciidoc-toc.css -a idprefix -o - $i | hxtoc -l 2 > ${i:r}.html &
+  asciidoc -b xhtml11 -f asciidoc.conf -a disable-javascript -a data-uri -a stylesheet=$PWD/asciidoc-toc.css -a idprefix -o - $i | hxtoc -l 2 > ${i:r}.html
 done
