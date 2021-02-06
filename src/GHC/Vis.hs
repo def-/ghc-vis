93d7d63ab8a960d24ea5b8dbf6234e07fe7bd904
Joachim Breitner <mail@joachim-breitner.de>
2021-02-06 09:43:29 +0100

    No not leave code comments

--- src/GHC/Vis.hs
+++ src/GHC/Vis.hs
@@ -309,7 +309,6 @@ setupGUI :: (WidgetClass w1, WidgetClass w2, WidgetClass w3) => w1 -> w2 -> w3 -
 setupGUI window canvas legendCanvas = do
   widgetAddEvents canvas [PointerMotionMask]
   on canvas motionNotifyEvent $ do
-    -- liftIO $ traceIO "on canvas motionNotifyEvent"
     (x,y) <- eventCoordinates
     lift $ do
       state <- readIORef visState
@@ -324,7 +323,6 @@ setupGUI window canvas legendCanvas = do
         widgetQueueDraw canvas
       else
         runCorrect move >>= \f -> f canvas
-      -- traceIO "on canvas motionNotifyEvent End"
       return True
 
   on canvas buttonPressEvent $ do
